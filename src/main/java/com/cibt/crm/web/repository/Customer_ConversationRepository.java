c0971211e25ec3dddd4a19a4f484c7a5567c94c1
sulavaaa <sulavaryal@gmail.com>
2019-05-08 08:47:32 +0545

    Added convo repo

---
+++ src/main/java/com/cibt/crm/web/repository/Customer_ConversationRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.repository;
+
+import com.cibt.crm.web.entity.Customer_Conversation;
+
+/**
+ *
+ * @author dale
+ */
+public interface Customer_ConversationRepository extends CrudRepository<Customer_Conversation> {
+    
+}
+
+
+
+
