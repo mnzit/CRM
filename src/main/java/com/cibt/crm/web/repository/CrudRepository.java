2095846bfc005afb4097a6b9cccd4b8fbb08b246
mnzit <mnzitshakya@gmail.com>
2019-05-08 08:23:36 +0545

    Generic interfaces added

---
+++ src/main/java/com/cibt/crm/web/repository/CrudRepository.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.repository;
+
+import java.util.List;
+
+/**
+ *
+ * @author Mnzit
+ * @param <T>
+ */
+public interface CrudRepository<T> {
+
+    void insert(T model) throws Exception;
+
+    void update(T model) throws Exception;
+
+    T getById(int id) throws Exception;
+
+    void delete(int id) throws Exception;
+
+    List<T> getAll() throws Exception;
+
+}
---
+++ src/main/java/com/cibt/crm/web/service/GenericService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.service;
+
+import java.util.List;
+
+/**
+ *
+ * @author Mnzit
+ */
+public interface GenericService<T> {
+
+    void save(T model) throws Exception;
+
+    void delete(int id) throws Exception;
+
+    T fetchById(int id) throws Exception;
+
+    List<T> fetchAll() throws Exception;
+}
