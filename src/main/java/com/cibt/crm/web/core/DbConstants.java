2053b46063478d9251ac18f932581bfd62fdb5fa
mnzit <mnzitshakya@gmail.com>
2019-05-08 08:13:35 +0545

    query added in template

---
+++ src/main/java/com/cibt/crm/web/core/DbConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.core;
+
+/**
+ *
+ * @author Mnzit
+ */
+public class DbConstants {
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    public static final String URL = "jdbc:mysql://localhost:3306/crm";
+}
--- src/main/java/com/cibt/crm/web/core/JdbcTempate.java
+++ src/main/java/com/cibt/crm/web/core/JdbcTempate.java
@@ -5,10 +5,31 @@
  */
 package com.cibt.crm.web.core;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Mnzit
  */
-public class JdbcTempate {
+public class JdbcTempate<T> {
+
+    public Connection getConnection() throws Exception {
+        return DriverManager.getConnection(DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
+    }
+
+    public void addParameters(PreparedStatement pstm, Object[] args) throws Exception {
+        int count = 1;
+        for (Object arg : args) {
+            pstm.setObject(count++, arg);
+        }
+    }
+
+    public int query(String sql, Object[] args) throws Exception {
+        PreparedStatement pstm = getConnection().prepareStatement(sql);
+        addParameters(pstm, args);
+        return pstm.executeUpdate();
+    }
     
 }
