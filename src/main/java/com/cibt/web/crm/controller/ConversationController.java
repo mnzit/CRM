5f0fda9db5be4a5bcc737f71c7912e7bce4f0a80
sulavaaa <sulavaryal@gmail.com>
2019-05-08 20:53:06 +0545

    added convo service and controller

---
+++ src/main/java/com/cibt/web/crm/controller/ConversationController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.web.crm.controller;
+
+/**
+ *
+ * @author dale
+ */
+public class ConversationController extends Controller{
+    
+}
