50dba28dc3b1d18f49bdeffb19d28d6f306946cf
sulavaaa <sulavaryal@gmail.com>
2019-05-08 10:29:00 +0545

    added Convo repo and it's impl

---
+++ src/main/java/com/cibt/crm/web/repository/impl/ConversationRepositoryImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.repository.impl;
+
+import com.cibt.crm.web.entity.Conversation;
+import com.cibt.crm.web.repository.ConversationRepository;
+import java.util.List;
+
+/**
+ *
+ * @author dale
+ */
+public class ConversationRepositoryImpl implements ConversationRepository {
+
+    @Override
+    public void insert(Conversation model) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Conversation model) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Conversation getById(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Conversation> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
