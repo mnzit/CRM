e23c865c4baf5308474aae3db872473e4c49306e
mnzit <mnzitshakya@gmail.com>
2019-05-08 08:07:32 +0545

    entity added customer

---
+++ src/main/java/com/cibt/crm/web/entity/Customer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crm.web.entity;
+
+/**
+ *
+ * @author Mnzit
+ */
+public class Customer {
+    
+}
